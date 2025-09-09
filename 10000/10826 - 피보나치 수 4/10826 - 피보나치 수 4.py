f617214b270c010f1a1895a9036cb0cc9a0b21e6
Cyan <hoony6134@gmail.com>
2025-09-09 16:31:08 +0900

    feat(boj/improvement): use bottom up for 10826

--- 10000/10826 - 피보나치 수 4/10826 - 피보나치 수 4.py
+++ 10000/10826 - 피보나치 수 4/10826 - 피보나치 수 4.py
@@ -6,18 +6,28 @@ input = sys.stdin.readline
 global l
 l = [None] * 10001
 
+# top-down
 def fib(n):
     if n<=1:
         l[n]=n
     if l[n] is None:
         l[n] = fib(n-1) + fib(n-2)
     return l[n]
-    
 
+# bottom-up
+def fib_bu(n):
+    if n==0:
+        return 0
+    li = [0] * (n+1)
+    li[1] = 1
+    for i in range(2,n+1):
+        li[i] = li[i-1] + li[i-2]
+    return li[n]
+    
 def main():
     sys.setrecursionlimit(20000)
     n = int(input())
-    print(fib(n))
+    print(fib_bu(n))
 
 if __name__ == "__main__":
     main()

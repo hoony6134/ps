7fe8da0f64b8bee1fa07ed092f0e23317b018479
Cyan <hoony6134@gmail.com>
2025-10-16 22:24:19 +0900

    docs: annotation for tabulation approach for 0/1 knapsack

--- 12000/12865 - 평범한 배낭/12865 - 평범한 배낭 - tabulation.py
+++ 12000/12865 - 평범한 배낭/12865 - 평범한 배낭 - tabulation.py
@@ -3,12 +3,14 @@ def knapSack(W, wt, val, n):
     # print(K)
     for i in range(n + 1):
         for w in range(W + 1):
-            if i == 0 or w == 0:
+            if i == 0 or w == 0:  # 양쪽 첫줄 0으로
                 K[i][w] = 0
-            elif wt[i - 1] <= w:
-                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w])
+            elif wt[i - 1] <= w:  # 이전거 무게가 해당 가방의 무게 제한보다 작으면
+                K[i][w] = max(
+                    val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]
+                )  # 이전거 가치 + 무게 제한에서 이전거 무게를 뺀 것의 가치 / 이전거 가치 중 큰 것
             else:
-                K[i][w] = K[i - 1][w]
+                K[i][w] = K[i - 1][w]  # 이전걸 현재 가치로 채택
     # print(K)
     return K[n][W]
 
